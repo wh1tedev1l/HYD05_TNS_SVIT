7190c55cf78e51859dc19b33b47b699deb0fd3a2
Lucifer <Lucifer@LAPTOP-LPIUVE49>
2023-09-10 13:52:19 +0530

    Day one Project

---
+++ DAY1/src/com/svit/dayone/datatypes/IdentifiersDemo.java
@@ -0,0 +1,16 @@
+package com.svit.dayone.datatypes;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Bala Sai"; 
+		System.out.println("value is : "+ studentName);
+		
+	
+	}
+
+}
