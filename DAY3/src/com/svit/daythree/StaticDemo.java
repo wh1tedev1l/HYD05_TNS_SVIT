f86395763ca4323470c228c939ac26f984799544
Lucifer <Lucifer@LAPTOP-LPIUVE49>
2023-09-12 20:29:07 +0530

    Day 3 Completed

---
+++ DAY3/src/com/svit/daythree/StaticDemo.java
@@ -0,0 +1,13 @@
+package com.svit.daythree;
+
+public class StaticDemo {
+	
+	static int a = 13; //static variable 
+	 static int b = 1199; // static variable
+	 int c=10; // instance varaible
+	 static void callme() 
+	{
+	System.out.println("The value of a = " + a);// static method can access static variable
+	}
+
+}
